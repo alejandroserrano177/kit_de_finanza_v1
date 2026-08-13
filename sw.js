bc773830132ef4d22081c1e91646dd55a4ee482b
alejandroserrano177 <alejandroserrano07@gmail.com>
2026-08-13 18:07:39 -0500

    Update service worker to version 4.0 with new assets

--- sw.js
+++ sw.js
@@ -1,25 +1,51 @@
-const CACHE_NAME="kit-finanzas-v3-0";
-const ASSETS=["./","./index.html","./style.css","./app.js"];
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+const CACHE_NAME = "kit-finanzas-v4-0";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon-192.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
-self.addEventListener("activate",event=>{
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET") return;
-  const url=new URL(event.request.url);
-  if(url.origin===self.location.origin){
-    event.respondWith(
-      fetch(event.request).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
         return response;
-      }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
-    );
-  }
+      })
+      .catch(() =>
+        caches.match(event.request).then(
+          cached => cached || caches.match("./index.html")
+        )
+      )
+  );
 });
